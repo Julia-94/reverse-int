aa53d0d4cc34de0d05a39e598228c26c08c8bfd5
Shaitanova_Julia <shaitanova.julia@gmail.com>
2023-02-27 12:27:05 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,6 @@
 module.exports = function reverse (n) {
-  
+    const arrayNum = Array.from(String(Math.abs(n)));
+    const arrayNumReverse = arrayNum.reverse();
+    const result = arrayNumReverse.join('') * 1;
+    return result;
 }
